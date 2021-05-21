b55b995b42349f2fb6639a24d6a5dc3453081cd5
ErikSCc <77497924+ErikSCc@users.noreply.github.com>
2021-05-21 05:53:50 +0200

    finished

--- app/src/main/java/com/example/myapplication/activity/RepositoryActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/RepositoryActivity.kt
@@ -3,7 +3,10 @@ package com.example.myapplication.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.adapter.RepositoryRecycleviewAdapter
 import com.example.myapplication.model.Repository
 
 class RepositoryActivity : AppCompatActivity() {
@@ -17,7 +20,19 @@ class RepositoryActivity : AppCompatActivity() {
             val numberOfRepositories=getString(R.string.number_of_repos,it.size)
 
            findViewById<TextView>(R.id.textViewNumberOfRepos)?.text=numberOfRepositories
+            showRepos(it)
         }
+    }
+    private fun showRepos(ListOfRepositories:ArrayList<Repository>){
+
+        val recycleViewAdapter=RepositoryRecycleviewAdapter(ListOfRepositories)
+        val recycleView=findViewById<RecyclerView>(R.id.recyclerview)
+        recycleView?.apply {
+            adapter=recycleViewAdapter
+            setHasFixedSize(true)
+            layoutManager=LinearLayoutManager(context)
+        }
+
     }
     companion object{
         const val KEY_REPOSITORY_DATA="keyRepositoryData"
